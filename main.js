03593c22ae71c771fe54ccfc3deb31f5e1d09afe
Giridhar8 <giridhar.c2004@gmail.com>
2025-11-14 13:17:19 +0530

    Updated Weather API

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const app = Vue.createApp({
       theme: localStorage.getItem("theme") || "light",
       tempChart: null,
       selectedChart: "bar",
-      API_KEY: "YOUR_API_KE14966ee8d6d4ce3893791db842069be8Y"
+      API_KEY: "14966ee8d6d4ce3893791db842069be8Y"
     };
   },
 
